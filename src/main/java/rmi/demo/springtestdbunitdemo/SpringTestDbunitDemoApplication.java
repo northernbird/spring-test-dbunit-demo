cd47e3821bceada866990139f39598d2e24f8d6a
northernbird <rie.miyazaki98@gmail.com>
2020-03-25 08:36:38 +0900

    feat: Initial commit

---
+++ src/main/java/rmi/demo/springtestdbunitdemo/SpringTestDbunitDemoApplication.java
@@ -0,0 +1,13 @@
+package rmi.demo.springtestdbunitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDbunitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestDbunitDemoApplication.class, args);
+	}
+
+}
