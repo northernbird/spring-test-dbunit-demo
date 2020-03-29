9833998160a0b61df98087d769a2bf58fff9a256
northernbird <northernbird98@gmail.com>
2020-03-30 08:37:22 +0900

    feat(IT(Controller)): Added auto generated date value assertion for DBUnit

--- src/main/java/rmi/demo/springtestdbunitdemo/db/entity/DemoEntity.java
+++ src/main/java/rmi/demo/springtestdbunitdemo/db/entity/DemoEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
-@Entity
+@Entity(name = "demo")
 @EntityListeners(AuditingEntityListener.class)
 public class DemoEntity extends BaseEntity {
 
